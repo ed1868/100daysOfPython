df2a0a050b0e05b413ebf6be7dd50b829101d281
ed1868 <ruizeduardo21@gmail.com>
2022-08-10 20:57:16 -0400

    day2

---
+++ Day2/main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("French to English")
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+# canvas =  Canvas(width=800, height=526)
+# card_front_img = PhotoImage(file="images/card_front.png")
+# canvas.create_image(400,263, image=card_front_img)
+#
+# canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+# canvas.grid(row=0,column=0)
+window.mainloop()
+
+
